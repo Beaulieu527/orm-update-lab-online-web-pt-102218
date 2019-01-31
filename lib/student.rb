aba884229b6cd5e996e0925d44afebf1610791fc
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-31 01:10:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
    new_student.id = row[0]
    new_student.name =  row[1]
    new_student.grade = row[2]
-   rnew_student  # return the newly created instance
+   new_student  # return the newly created instance
   end
 
   def self.find_by_name(name)
