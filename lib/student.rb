ee43fc3c6ad391efeba494826e9fee6a34a3f1fc
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-31 01:04:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,11 @@ class Student
   attr_accessor :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(id = nil, name, grade)
-    @id = id
+  def initialize(name, grade, id = nil)
+  
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.drop_table
