2f9ad481206cdeb6337f7babbf50408bd040e940
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-31 00:29:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,21 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+  end
+
+  def self.drop_table
+  end
+
+  def self.create_table
+  end
+
+  def self.new_from_db
+  end
+
+  def self.find_by_name
+  end
+
+
+
 end
