4afd9edc3844ec260d1cfafd6f7790d28b218d0f
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-31 01:11:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
   end
 
   def self.new_from_db(row)
-   new_student = Student.new  # self.new is the same as running Student.new
+   new_student = Student.new(name, grade, id)  # self.new is the same as running Student.new
    new_student.id = row[0]
    new_student.name =  row[1]
    new_student.grade = row[2]
