cde48a3c2f0a8598213d37eeaad64a3b76dc4d0f
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-31 00:45:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,9 +37,15 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM studentss WHERE name = ?"
-     result = DB[:conn].execute(sql, name)[0]
-     Student.new(result[0], result[1], result[2])
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def update
